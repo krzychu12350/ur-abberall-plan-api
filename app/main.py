752610bcdaa19cb4290fddbd57b79ae60d0d1b22
Krzysztof Karaś <krzychu12350@interia.pl>
2024-09-24 00:32:51 +0200

    Working fastapiv4

--- app/main.py
+++ app/main.py
@@ -6,12 +6,21 @@ from .routers import system
 app = FastAPI()
 app.include_router(system.router, prefix="/system")
 
+
 @app.get("/statuss")
 def status():
     return {"ok": True, "version": __version__}
+
 
 @app.post("/hello")
 async def hello_message(dto: ISayHelloDto):
     return {
         "Filenames": [file.filename for file in dto.files],
     }
+
+
+@app.get("/api/plan")
+async def ping():
+    data = [1, 2, 3, 4, 5]
+    # print(data)
+    return {"data": data}
