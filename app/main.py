e39c05fc5de8e36fa75bc05dae353f9819a7dde6
Krzysztof Karaś <krzychu12350@interia.pl>
2024-09-25 21:25:43 +0200

    Small update

--- app/main.py
+++ app/main.py
@@ -183,18 +183,18 @@ async def hello_message(dto: ISayHelloDto):
     }
 
 
-@app.get("/api/schedules", response_model=[])
+@app.get("/api/schedules", response_model=Dict[str, List[Dict[str, Any]]])
 async def get_schedules():
-    # data = extract_data()
-    #
-    # # Group schedules by date
-    # grouped_schedules = defaultdict(list)
-    #
-    # for schedule in data:
-    #     grouped_schedules[schedule['date']].append(schedule)
-    #
-    # # Sort the grouped schedules by date
-    # sorted_grouped_schedules = dict(
-    #     sorted(grouped_schedules.items(), key=lambda x: datetime.strptime(x[0], '%d.%m.%Y')))
+    data = extract_data()
 
-    return [1, 2, 3]
+    # Group schedules by date
+    grouped_schedules = defaultdict(list)
+
+    for schedule in data:
+        grouped_schedules[schedule['date']].append(schedule)
+
+    # Sort the grouped schedules by date
+    sorted_grouped_schedules = dict(
+        sorted(grouped_schedules.items(), key=lambda x: datetime.strptime(x[0], '%d.%m.%Y')))
+
+    return sorted_grouped_schedules
